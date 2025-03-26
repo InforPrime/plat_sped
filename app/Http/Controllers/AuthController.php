6d48e7a97d01fb3c0c2807922e9a718793e343c8
InforPrime Nova Cruz <inforprimenc@gmail.com>
2025-03-26 14:03:40 -0300

    Test: Adicionando testes do login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(LoginRequest $request)
     {
         $credentials = $request->only('email', 'password');
         if (auth()->attempt($credentials) && auth()->user()->role === 'admin') {
