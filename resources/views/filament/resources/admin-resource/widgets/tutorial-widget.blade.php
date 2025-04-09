f8110a87784c46ccb8c0b5655750ba348bb7a847
InforPrime Nova Cruz <inforprimenc@gmail.com>
2025-04-09 15:05:17 -0300

    Fix: texto do tutorial não aparecia no dark mode

--- resources/views/filament/resources/admin-resource/widgets/tutorial-widget.blade.php
+++ resources/views/filament/resources/admin-resource/widgets/tutorial-widget.blade.php
@@ -1,22 +1,22 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <h2 class="text-xl font-bold">📌 Tutorial de Uso</h2>
-        <p class="mt-2 text-gray-600">
+        <p class="mt-2 font-bold">
             Bem-vindo ao sistema! Aqui está um breve guia para ajudá-lo:
         </p>
 
-        <ul class="mt-4 space-y-2 text-gray-700">
+        <ul class="mt-4 space-y-2 font-bold">
             <li>✅ Vá para a aba de <strong>Clientes</strong> onde você encontrará seus clientes.</li>
             <li>✅ Selecione um para ver os seus arquivos.</li>
             <li>✅ Clique em <strong>Download</strong> para baixar o determinado arquivo no seu computador.</li>
             <li>✅ Qualquer dúvida, consulte o suporte.</li>
         </ul>
         <h2 class="text-xl font-bold mt-2">📌 Recomendamos</h2>
-        <p class="mt-2 text-gray-600">
+        <p class="mt-2 font-bold">
             Recomendamos que você altere a sua senha de acesso ao sistema.
             Para isso:
         </p>
-        <ul class="mt-4 space-y-2 text-gray-700">
+        <ul class="mt-4 space-y-2 font-bold">
             <li>✅ clique no seu nome no canto superior direito e vá em <strong>Configurações</strong>.</li>
             <li>✅ Ou clique em <strong>Users</strong> e selecione o seu Usuário.</strong></li>
         </ul>
